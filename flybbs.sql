ecfce1902e4e0fb09c7a5242e64363d80ad07a81
your name <your email>
2018-12-26 13:54:44 +0800

    完善

--- flybbs.sql
+++ flybbs.sql
@@ -156,7 +156,7 @@ INSERT INTO `tab_topic_category` VALUES ('5', '公告');
 INSERT INTO `tab_topic_category` VALUES ('6', '动态');
 
 
-alter table tab_user add COLUMN isadmin int(1) NOT NULL DEFAULT 0
+alter table tab_user add COLUMN isadmin int(1) NOT NULL DEFAULT 0;
 
 create table tab_user_comment_agree(
 	id int PRIMARY key not null AUTO_INCREMENT,
